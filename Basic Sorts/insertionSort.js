f3070dfdc7f1b8a5ffd5e3dcf540baf00ddd1102
Sameer Sondhi <110759750+SameerSondhi@users.noreply.github.com>
2023-04-12 15:30:18 -0700

    Add files via upload

---
+++ Basic Sorts/insertionSort.js
@@ -0,0 +1,28 @@
+function insertionSort(array){
+    for(let i =1; i<array.length; i++){
+        temp = array[i]
+        for(var j =i-1; array[j]>temp && j>-1; j--){
+            array[j+1] = array[j]
+        }
+        array[j+1] = temp
+    }
+    return temp
+}
+ 
+
+function test() {
+    let myArray = [4,2,6,5,1,3];
+    insertionSort(myArray);
+    console.log(myArray);
+}
+
+
+test();
+
+
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    [ 1, 2, 3, 4, 5, 6 ]
+
+*/  
